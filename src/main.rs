70ca9b753bd3ce530131ce89199f7171d86b047f
Gaeric <1062217098@qq.com>
2024-09-20 08:28:51 +0800

    delete phong module for clear code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use winit::{event_loop::EventLoop, window::WindowBuilder};
 
+use rtr_tutorial::PhongState;
+
 async fn exec() {
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
-    let mut state = phong::PhongState::new(&window).await.unwrap();
+    let mut state = PhongState::new(&window).await.unwrap();
     common::run(&mut state, event_loop).await;
 }
 
