7e76970f1ac327ca248d2158ca9cb6847f91d441
Gaeric <1062217098@qq.com>
2024-10-11 16:39:49 +0800

    adding a progress indicator for cc/rs project

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,18 @@
+use core::time;
+use std::{io::Write, thread::sleep};
+
 fn main() {
     const IMAGE_WIDTH: i32 = 256;
     const IMAGE_HEIGHT: i32 = 256;
 
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
+    let ms = time::Duration::from_millis(10);
 
-    for i in 0..IMAGE_HEIGHT {
-        for j in 0..IMAGE_WIDTH {
+    for j in 0..IMAGE_HEIGHT {
+        eprint!("\rScanlines remaining: {}", IMAGE_HEIGHT - j);
+        std::io::stderr().flush().unwrap();
+        sleep(ms);
+        for i in 0..IMAGE_WIDTH {
             let r = i as f64 / (IMAGE_WIDTH - 1) as f64;
             let g = j as f64 / (IMAGE_WIDTH - 1) as f64;
             let b = 0.0 as f64;
@@ -17,4 +24,6 @@ fn main() {
             println!("{ir} {ig} {ib}");
         }
     }
+
+    eprintln!("\rDone.        ");
 }
