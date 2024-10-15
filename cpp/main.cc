cfcb4ce99580508d20f3d3ae1d37fd1107772ab3
Gaeric <1062217098@qq.com>
2024-10-15 09:09:16 +0800

    simplifying the ray-sphere function

--- cpp/main.cc
+++ cpp/main.cc
@@ -6,15 +6,17 @@
 
 double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = center - r.origin();
-    auto a = dot(r.direction(), r.direction());
-    auto b = -2.0 * dot(r.direction(), oc);
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
+    auto a = r.direction().length_squared();
+    // auto b = -2.0 * dot(r.direction(), oc);
+    // b = -2h => t = (h+-sqrt(h^2 - 4ac)) / a
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = h * h - a * c;
 
     if (discriminant < 0) {
         return -1.0;
     } else {
-        return (-b - std::sqrt(discriminant)) / (2.0 * a);
+        return (h - std::sqrt(discriminant)) / a;
     }
 }
 
@@ -23,7 +25,7 @@ color ray_color(const ray& r) {
     auto t = hit_sphere(center, 0.5, r);
     if (t > 0.0) {
         vec3 N = unit_vector(r.at(t) - center);
-        return 0.5*color(N.x() + 1, N.y() + 1, N.z() + 1);
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
