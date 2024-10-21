94f0c04c0e8028291ccffbb9a39ec47acb3a1edf
Gaeric <1062217098@qq.com>
2024-10-21 11:52:21 +0800

    add dir-local variables for emacs

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((c++-ts-mode . ((c-ts-mode-indent-offset . 4)))
+ (c-ts-mode . ((c-ts-mode-indent-offset . 4))))
