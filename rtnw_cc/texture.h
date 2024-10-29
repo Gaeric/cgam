09a40932fd4ef4404de1ae38b04fc2cc95e95381
Gaeric <1062217098@qq.com>
2024-10-29 09:01:30 +0800

    add checkered texture

--- rtnw_cc/texture.h
+++ rtnw_cc/texture.h
@@ -1,6 +1,7 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
+#include <memory>
 #include "color.h"
 
 class texture {
@@ -19,6 +20,29 @@ class solid_color : public texture {
 
    private:
     color albedo;
+};
+
+class checker_texture : public texture {
+   public:
+    checker_texture(double scale, shared_ptr<texture> even, shared_ptr<texture> odd)
+        : inv_scale(1.0 / scale), even(even), odd(odd) {}
+
+    checker_texture(double scale, const color& c1, const color& c2)
+        : checker_texture(scale, make_shared<solid_color>(c1), make_shared<solid_color>(c2)) {}
+
+    color value(double u, double v, const point3& p) const override {
+        auto x_int = int(std::floor(inv_scale * p.x()));
+        auto y_int = int(std::floor(inv_scale * p.y()));
+        auto z_int = int(std::floor(inv_scale * p.z()));
+
+        bool is_even = (x_int + y_int, +z_int) % 2 == 0;
+        return is_even ? even->value(u, v, p) : odd->value(u, v, p);
+    }
+
+   private:
+    double inv_scale;
+    shared_ptr<texture> even;
+    shared_ptr<texture> odd;
 };
 
 #endif
