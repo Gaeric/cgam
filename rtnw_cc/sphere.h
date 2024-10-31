17b04ac328d00e37fdd999340f77c3a30718b0b2
Gaeric <1062217098@qq.com>
2024-10-31 08:55:14 +0800

    sphere_uv function

--- rtnw_cc/sphere.h
+++ rtnw_cc/sphere.h
@@ -16,7 +16,7 @@ class sphere : public hittable {
 
     // Moving Sphere
     sphere(const point3& center1, const point3& center2, double radius, shared_ptr<material> mat)
-    : center(center1, center2 - center1), radius(std::fmax(0, radius)), mat(mat) {
+        : center(center1, center2 - center1), radius(std::fmax(0, radius)), mat(mat) {
         auto rvec = vec3(radius, radius, radius);
         aabb box1(center.at(0) - rvec, center.at(0) + rvec);
         aabb box2(center.at(1) - rvec, center.at(1) + rvec);
@@ -51,12 +51,25 @@ class sphere : public hittable {
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - current_center) / radius;
         rec.set_face_normal(r, outward_normal);
+        get_sphere_uv(outward_normal, rec.u, rec.v);
         rec.mat = mat;
 
         return true;
     }
 
     aabb bounding_box() const override { return bbox; }
+
+    static void get_sphere_uv(const point3& p, double& u, double& v) {
+        // p: a given point on the sphere of radius one, cnetered at the origin.
+        // u: return value [0, 1] of angle around the Y axis from X = -1.
+        // v: return value [0, 1] of angle from Y=-1 to Y=+1.
+
+        auto theta = std::acos(-p.y());
+        auto phi = std::atan2(-p.z(), p.x()) + pi;
+
+        u = phi / (2 * pi);
+        v = theta / pi;
+    }
 
    private:
     ray center;
