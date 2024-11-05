cd45bdc073c22776325bf5772dbfbd65bea8ae96
Gaeric <1062217098@qq.com>
2024-11-05 08:24:16 +0800

    rtnw_cc tweaking the frequency

--- rtnw_cc/main.cc
+++ rtnw_cc/main.cc
@@ -1,5 +1,4 @@
 #include <chrono>
-#include <memory>
 #include "bvh.h"
 #include "camera.h"
 #include "hittable_list.h"
@@ -118,7 +117,7 @@ void earth() {
 
 void perlin_spheres() {
     hittable_list world;
-    auto pertext = make_shared<noise_texture>();
+    auto pertext = make_shared<noise_texture>(4);
     world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
     world.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
 
