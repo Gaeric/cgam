097b04795fbc5c62475c28444e1b77240050c381
Gaeric <1062217098@qq.com>
2024-11-15 08:25:09 +0800

    sampling areas with jittered points

--- rtrl_cc/main.cc
+++ rtrl_cc/main.cc
@@ -373,20 +373,30 @@ void estimating_pi() {
     std::cout << std::fixed << std::setprecision(12);
 
     int inside_circle = 0;
-    int runs = 0;
+    int inside_circle_stratified = 0;
+    int sqrt_N = 1000;
 
-    while (true) {
-        runs++;
-        auto x = random_double(-1, 1);
-        auto y = random_double(-1, 1);
-        if (x * x + y * y < 1) {
-            inside_circle++;
-        }
+    for (int i = 0; i < sqrt_N; i++) {
+        for (int j = 0; j < sqrt_N; j++) {
+            auto x = random_double(-1, 1);
+            auto y = random_double(-1, 1);
 
-        if (runs % 100000 == 0) {
-            std::cout << "\rEstimate of Pi = " << (4.0 * inside_circle) / runs;
+            if (x * x + y * y < 1) {
+                inside_circle++;
+            }
+
+            // x = 2 * ((i + random_double()) / sqrt_N) - 1;
+            // y = 2 * ((j + random_double()) / sqrt_N) - 1;
+            x = ((2 * i + random_double() + 1) / sqrt_N) - 1;
+            y = ((2 * j + random_double() + 1) / sqrt_N) - 1;
+
+            if (x * x + y * y < 1) {
+                inside_circle_stratified++;
+            }
         }
     }
+    std::cout << "Regular    Estimate of Pi = " << (4.0 * inside_circle) / (sqrt_N * sqrt_N) << "\n";
+    std::cout << "Stratified Estimate of Pi = " << (4.0 * inside_circle_stratified) / (sqrt_N * sqrt_N) << "\n";
 }
 
 typedef enum {
