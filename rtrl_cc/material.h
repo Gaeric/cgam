ace8d4a3013feb89d3cbe73a665b4d8cba5fb9d8
Gaeric <1062217098@qq.com>
2024-12-02 08:40:37 +0800

    rtrl_cc: material emission with ray_color change

--- rtrl_cc/material.h
+++ rtrl_cc/material.h
@@ -11,7 +11,9 @@ class material {
    public:
     virtual ~material() = default;
 
-    virtual color emitted(double u, double v, const point3& p) const { return color(0, 0, 0); }
+    virtual color emitted(const ray& r_in, const hit_record& rec, double u, double v, const point3& p) const {
+        return color(0, 0, 0);
+    }
 
     virtual bool scatter(const ray& r_in,
                          const hit_record& rec,
@@ -129,7 +131,16 @@ class diffuse_light : public material {
     diffuse_light(shared_ptr<texture> tex) : tex(tex) {}
     diffuse_light(const color& emit) : tex(make_shared<solid_color>(emit)) {}
 
-    color emitted(double u, double v, const point3& p) const override { return tex->value(u, v, p); }
+    color emitted(const ray& r_in,
+                  const hit_record& rec,
+                  double u,
+                  double v,
+                  const point3& p) const override {
+        if (!rec.front_face) {
+            return color(0, 0, 0);
+        }
+        return tex->value(u, v, p);
+    }
 
    private:
     shared_ptr<texture> tex;
