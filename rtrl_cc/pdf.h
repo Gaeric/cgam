c0cf8e4cc4caccb41a0b7ec9431197a255abc072
Gaeric <1062217098@qq.com>
2024-12-02 08:54:27 +0800

    rtrl_cc: add the sphere_pdf class

--- rtrl_cc/pdf.h
+++ rtrl_cc/pdf.h
@@ -2,6 +2,7 @@
 #define PDF_H
 
 #include "onb.h"
+#include "vec3.h"
 
 class pdf {
    public:
@@ -9,6 +10,15 @@ class pdf {
 
     virtual double value(const vec3& direction) const = 0;
     virtual vec3 generate() const = 0;
+};
+
+class sphere_pdf : public pdf {
+   public:
+    sphere_pdf() {}
+
+    double value(const vec3& direction) const override { return 1 / (4 * pi); }
+
+    vec3 generate() const override { return random_unit_vector(); }
 };
 
 #endif
