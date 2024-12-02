43fb3134b659b9df5f21dc8000c88a2aacb83ef1
Gaeric <1062217098@qq.com>
2024-12-02 08:51:33 +0800

    rtrl_cc: the abstrace pdf class

---
+++ rtrl_cc/pdf.h
@@ -0,0 +1,14 @@
+#ifndef PDF_H
+#define PDF_H
+
+#include "onb.h"
+
+class pdf {
+   public:
+    virtual ~pdf() {}
+
+    virtual double value(const vec3& direction) const = 0;
+    virtual vec3 generate() const = 0;
+};
+
+#endif
