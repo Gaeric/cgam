b53763c15fbad0f36c22428ae3ee24b2c853fb9c
Gaeric <1062217098@qq.com>
2025-01-06 19:15:28 +0800

    add connect to gpu function

--- rtow_wgpu/src/main.rs
+++ rtow_wgpu/src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{self, EventLoop};
@@ -52,4 +52,60 @@ fn main() -> Result<()> {
     let _ = event_loop.run_app(&mut app);
 
     Ok(())
+}
+
+async fn connect_to_gpu(window: &Window) -> Result<(wgpu::Device, wgpu::Queue, wgpu::Surface)> {
+    // Create an "instance" of wgpu. This is the entry-point to the API.
+    let instance = wgpu::Instance::default();
+
+    // Create a drawable "surface" that is associated with the window.
+    let surface = instance.create_surface(window)?;
+
+    // Request a GPU that is compatible with the surface. If the system has multiple GPUs then
+    // pick the hight performance one.
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            force_fallback_adapter: false,
+            compatible_surface: Some(&surface),
+        })
+        .await
+        .context("failed to faind a compatible adapter")?;
+
+    // Connect to the GPU. "device" represents the connection to the GPU and allows us to create
+    // resources like buffers, textures, and pipelines. "queue" represents the command queue that
+    // we use to submit commands to the GPU.
+    let (device, queue) = adapter
+        .request_device(&wgpu::DeviceDescriptor::default(), None)
+        .await
+        .context("failed to connect to the GPU")?;
+
+    // Configure the texture memory backs the surface. Our renderer will draw to a surface texture
+    // every frame.
+    let caps = surface.get_capabilities(&adapter);
+    let format = caps
+        .formats
+        .into_iter()
+        .find(|it| {
+            matches!(
+                it,
+                wgpu::TextureFormat::Rgba8Unorm | wgpu::TextureFormat::Bgra8Unorm
+            )
+        })
+        .context("could not find preferred texture format (Rgba8Unorm or Bgra8Unorm)")?;
+
+    let size = window.inner_size();
+    let config = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format,
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::AutoVsync,
+        alpha_mode: caps.alpha_modes[0],
+        view_formats: vec![],
+        desired_maximum_frame_latency: 3,
+    };
+    surface.configure(&device, &config);
+
+    Ok((device, queue, surface))
 }
